002d3e4f636089e65b49370c9204505abb7e5860
atomu-cf <atomu.cf@gmail.com>
2014-12-16 18:56:31 +0900

    bootstrap

--- minuti/db/schema.rb
+++ minuti/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216054002) do
+ActiveRecord::Schema.define(version: 20141216065311) do
+
+  create_table "groups", force: true do |t|
+    t.integer  "groups_id"
+    t.text     "groups_name"
+    t.integer  "logins_id"
+    t.integer  "users_id"
+    t.integer  "floortype_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.integer  "users_id"
