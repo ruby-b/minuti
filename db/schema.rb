d0e3b85c9ae1171741831d657d8c7b1323f80ade
atomu-cf <atomu.cf@gmail.com>
2015-02-17 13:42:16 +0900

    relationing models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216065311) do
+ActiveRecord::Schema.define(version: 20150217041218) do
 
   create_table "groups", force: true do |t|
     t.integer  "groups_id"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20141216065311) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "groups_users", id: false, force: true do |t|
+    t.integer "group_id", null: false
+    t.integer "user_id",  null: false
+  end
+
+  add_index "groups_users", ["group_id"], name: "index_groups_users_on_group_id"
+  add_index "groups_users", ["user_id"], name: "index_groups_users_on_user_id"
 
   create_table "users", force: true do |t|
     t.integer  "users_id"
