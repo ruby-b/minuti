4798deaa6b3dae080d773870286f19c2f8e5512b
atomu-cf <atomu.cf@gmail.com>
2015-02-17 17:44:23 +0900

    group users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,9 +30,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
+    gparams = params.require(:user).require(:group_users).permit(:id)
+    group = Group.find(gparams[:id].to_i)
+    @user.groups << group
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      if @user.save 
+       	format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
