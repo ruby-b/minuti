f1519b0e4b1c78a7f6450de8449f5c987512597c
atomu-cf <atomu.cf@gmail.com>
2015-05-26 17:00:50 +0900

    japanese

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317054441) do
+ActiveRecord::Schema.define(version: 20150526055720) do
 
   create_table "group_users", id: false, force: true do |t|
     t.integer "group_id", null: false
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20150317054441) do
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "minuts_users", force: true do |t|
+    t.integer "minut_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
