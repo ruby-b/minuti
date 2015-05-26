b675eb39dcb9995da7cefeada785cb1b0e17595d
Katsushi06 <patayan6@gmail.com>
2015-05-26 16:42:09 +0900

    2015/05/26

--- app/models/minut.rb
+++ app/models/minut.rb
@@ -1,5 +1,5 @@
 class Minut < ActiveRecord::Base
-    has_many :minuti_details, dependent: :destroy
+    has_and_belongs_to_many :users
 
     validates :title, presence: true
     validates :title, length: {minimum:3, maximum: 50, message:'3文字以上・50文字以下としてください'}
